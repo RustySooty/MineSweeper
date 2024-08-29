5c43e8834688177036326fa9bb92ab4794fee9fd
RustySooty <113718742+RustySooty@users.noreply.github.com>
2024-08-29 18:44:18 +0800

    Add files via upload

---
+++ MineMap.java
@@ -0,0 +1,23 @@
+package minesweeper;
+
+/**
+ * Locations of Mines
+ */
+
+public class MineMap {
+
+    boolean[][] isMined;
+
+    // Constructor
+    public MineMap(double diffThreshold, int rows, int cols) {
+        isMined = new boolean[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                double randomNumber = Math.random();
+                if (randomNumber < diffThreshold) {
+                    isMined[row][col] = true;
+                }
+            }
+        }
+    }
+}
